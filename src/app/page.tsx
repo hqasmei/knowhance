bc1cbeb8dae50c01b35d6930ea494bd8573f10ae
Hosna Qasmei <hosna.qasmei@gmail.com>
2023-06-07 11:22:38 -0700

    Made knowhance navigate to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import PeopleGrid from "@/components/people-grid";
 import Image from "next/image";
+import Link from "next/link";
 
 import Icon from "@/public/logo.svg";
 
@@ -7,10 +8,10 @@ export default function Home() {
   return (
     <main>
       <section className="items-center flex flex-col justify-center pt-24">
-        <div className="flex flex-row space-x-2 items-center mb-2">
+        <Link href="/" className="flex flex-row space-x-2 items-center mb-2">
           <Image src={Icon} alt="" width={40} height={40} />
           <h1 className="text-2xl font-semibold">Knowhance</h1>
-        </div>
+        </Link>
 
         <PeopleGrid />
       </section>
