1214005ddff96dcdb38275fec170f7d64081f708
Hosna Qasmei <hosna.qasmei@gmail.com>
2024-07-11 10:11:10 -0700

    updated to test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,3 @@
-import Image from "next/image";
-
-import Icon from "@/public/logo.svg";
-import PeopleGrid from "@/components/people-grid";
-
-import { getAllPeople } from "@/actions/getAllPeople";
-
-export default async function Home() {
-  const { people } = await getAllPeople();
-
-  return (
-    <main>
-      <section className="items-center flex flex-col justify-center pt-24">
-        <div className="flex flex-row space-x-2 items-center mb-2">
-          <Image src={Icon} alt="" width={40} height={40} />
-          <h1 className="text-2xl font-semibold">Knowhance</h1>
-        </div>
-
-        <PeopleGrid people={people} />
-      </section>
-    </main>
-  );
+export default function Home() {
+  return <main>hello world</main>;
 }
