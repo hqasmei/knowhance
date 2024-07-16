ab137310dab7d087e0f4c1ebe007663345b1549e
Hosna Qasmei <hosna.qasmei@gmail.com>
2024-07-15 23:48:39 -0700

    Testing this

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,30 @@ const nextConfig = {
       allowedOrigins: ["app.localhost:3000"],
     },
   },
+  async rewrites() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "admin.knowhance.com",
+          },
+        ],
+        destination: "/admin/:path*",
+      },
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "app.knowhance.com",
+          },
+        ],
+        destination: "/app/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,39 +1,15 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
-export const config = {
-  matcher: ["/((?!api/|_next/|_static/|_vercel|[\\w-]+\\.\\w+).*)"],
-};
+export function middleware(request: NextRequest) {
+  const url = request.nextUrl.clone();
+  const { hostname } = new URL(request.url);
 
-export default async function middleware(req: NextRequest) {
-  const url = req.nextUrl;
-  const hostname = req.headers
-    .get("host")!
-    .replace(".localhost:3000", `.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`);
-  const searchParams = req.nextUrl.searchParams.toString();
-  const path = `${url.pathname}${
-    searchParams.length > 0 ? `?${searchParams}` : ""
-  }`;
-
-  // Redirect /admin to admin.knowhance.com
-  if (path === "/admin") {
-    return NextResponse.redirect(new URL(`https://admin.${hostname}`, req.url));
+  if (hostname === "admin.knowhance.com") {
+    url.pathname = `/admin${url.pathname}`;
+  } else if (hostname === "app.knowhance.com") {
+    url.pathname = `/app${url.pathname}`;
   }
 
-  // Redirect /app to app.knowhance.com
-  if (path === "/app") {
-    return NextResponse.redirect(new URL(`https://app.${hostname}`, req.url));
-  }
-
-  // Rewrite root application to /home folder
-  if (
-    hostname === "localhost:3000" ||
-    hostname === process.env.NEXT_PUBLIC_ROOT_DOMAIN
-  ) {
-    return NextResponse.rewrite(
-      new URL(`/home${path === "/" ? "" : path}`, req.url)
-    );
-  }
-
-  // Rewrite everything else to /[domain]/[slug] dynamic route
-  return NextResponse.rewrite(new URL(`/${hostname}${path}`, req.url));
+  return NextResponse.rewrite(url);
 }
